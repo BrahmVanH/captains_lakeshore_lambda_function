b65a334af4b7a842af6bf35d38b2c756a0f75ab7
Brahm Van Houzen <brahmvanh@gmail.com>
2024-03-30 15:13:02 -0400

    finished upload to s3 flow

--- src/utils/s3Upload.ts
+++ src/utils/s3Upload.ts
@@ -1,11 +1,5 @@
-import https from 'https';
 import { PutObjectCommand, DeleteObjectCommand, S3Client } from '@aws-sdk/client-s3';
-import { fromIni } from '@aws-sdk/credential-providers';
-import { HttpRequest } from '@smithy/protocol-http';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { parseUrl } from '@smithy/url-parser';
-import { formatUrl } from '@aws-sdk/util-format-url';
-import { Hash } from '@smithy/hash-node';
 
 const createPresignedUrlWithClient = ({ region, bucket, key, commandType }: { region: string; bucket: string; key: string; commandType: string }) => {
 	const client = new S3Client({ region });
