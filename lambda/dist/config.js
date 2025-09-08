649423db040029ea60e33014ac86cf8a62a46fa4
Brahm Van Houzen <brahmvanh@gmail.com>
2025-09-07 22:23:19 -0400

    deleted some

--- lambda/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    s3ImagePrefixes: ["captains_hideaway_png", "captains_mackinac_lookout", "captains_cottage_png"]
-};
-//# sourceMappingURL=config.js.map
--- lambda/dist/graphql/schema/inputs/image-inputs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.imageInputs = void 0;
-exports.imageInputs = `
-    input DeleteS3ObjectInput {
-        imgKeys: [String!]!
-    }
-`;
-//# sourceMappingURL=image-inputs.js.map
